598a815d721a6f7902af56c1dbc420f38174eb1a
Murat AKALAN <murat@vskor.net>
2024-02-27 22:27:15 +0300

    Delete Note Capability Added Refactor/f0007

--- Project 01 - ToDoList/ToDoList/ViewController.swift
+++ Project 01 - ToDoList/ToDoList/ViewController.swift
@@ -7,60 +7,89 @@
 
 import UIKit
 
+class TodoManager {
+    static let shared = TodoManager()
+    
+    private let userDefaults = UserDefaults.standard
+    private let todoKey = "items"
+    
+    func fetchItems() -> [String] {
+        return userDefaults.stringArray(forKey: todoKey) ?? []
+    }
+    
+    func addItem(_ item: String) {
+        var currentItems = fetchItems()
+        currentItems.append(item)
+        userDefaults.set(currentItems, forKey: todoKey)
+    }
+    
+    func removeItem(at index: Int) {
+        var currentItems = fetchItems()
+        currentItems.remove(at: index)
+        userDefaults.set(currentItems, forKey: todoKey)
+    }
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
-    
-    private let table: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        return table
-    }()
-    
-    var items = [String]()
+    private let table = UITableView()
+    private var items = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        items = TodoManager.shared.fetchItems()
         title = "To Do List"
-        view.addSubview(table)
+        
+        setupTableView()
+        setupNavigationBar()
+    }
+    
+    private func setupTableView() {
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         table.dataSource = self
+        table.delegate = self // Added delegate
+        view.addSubview(table)
+        table.frame = view.bounds
+    }
+    
+    private func setupNavigationBar() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
     
     @objc private func didTapAdd() {
-        let alert = UIAlertController(title: "New Item", message: "Enter new to do list item!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "New Item", message: "Enter new to-do list item!", preferredStyle: .alert)
         
-        alert.addTextField {field in
+        alert.addTextField { field in
             field.placeholder = "Enter Item..."
         }
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self](_) in
-            if let field = alert.textFields?.first {
-                if let text = field.text, !text.isEmpty {
-                    DispatchQueue.main.async {
-                        var currenItems =  UserDefaults.standard.stringArray(forKey: "items") ?? []
-                        currenItems.append(text)
-                        UserDefaults.standard.setValue(currenItems, forKey: "items")
-                        self?.items.append(text)
-                        self?.table.reloadData()
-                    }
-                }
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] _ in
+            if let item = alert.textFields?.first?.text, !item.isEmpty {
+                TodoManager.shared.addItem(item)
+                self?.items.append(item)
+                self?.table.reloadData()
             }
         }))
+        
         present(alert, animated: true)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        table.frame = view.bounds
-    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
         cell.textLabel?.text = items[indexPath.row]
         return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            TodoManager.shared.removeItem(at: indexPath.row)
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
 }
